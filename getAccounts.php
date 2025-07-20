e9872dfdfce5bca7fadb4c97d58c5ab48dc92125
Mohamed Gamal <m.g.mohamed0123@gmail.com>
2025-07-20 19:13:44 +0200

    Update getAccounts.php

--- getAccounts.php
+++ getAccounts.php
@@ -1,1 +1,20 @@
+<?php
+header('Content-Type: application/json');
+require_once '../config.php';
 
+$sql = "SELECT a.*, at.name as type_name 
+        FROM accounts a
+        JOIN account_types at ON a.type_id = at.id
+        ORDER BY a.code";
+$result = $conn->query($sql);
+
+$accounts = [];
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $accounts[] = $row;
+    }
+}
+
+echo json_encode($accounts);
+$conn->close();
+?>
