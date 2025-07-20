3b8b089b7b7416b74e01341e53d062a281653509
Mohamed Gamal <m.g.mohamed0123@gmail.com>
2025-07-20 18:57:53 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "accounting_system";
 
+// إنشاء الاتصال
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// التحقق من الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال: " . $conn->connect_error);
+}
+
+// تعيين الترميز إلى UTF-8 لدعم اللغة العربية
+$conn->set_charset("utf8");
+?>
